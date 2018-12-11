14efde9583c40747e3faadc0bab76894daec7b6f
Andres Montenegro <41881835+andressmm4@users.noreply.github.com>
2018-12-11 09:59:17 -0600

    Vista de datos en Reservaciones
    Vista en tiempo real

---
+++ start/model/printFact.php
@@ -0,0 +1,33 @@
+<?php
+require 'connection.php';
+
+
+  $_SELECTreservations_SQL = "SELECT reservations.id_rev, reservations.name, reservations.num_person, reservations.consumo_asigned, reservations.table_asigned, reservations.fact, consumo.total_cost
+  FROM reservations, consumo WHERE reservations.consumo_asigned = consumo.id_cons AND reservations.fact = '1'
+  ORDER BY reservations.id_rev ASC";
+
+  $consultShow = mysqli_query($con, $_SELECTreservations_SQL);
+  
+
+  while ($resultRev = mysqli_fetch_array($consultShow)) {
+  ?>
+    <tr>
+      <td>
+        <?php print $resultRev['id_rev'];?>
+      </td>
+      <td>
+        <?php print $resultRev['name']; ?>
+      </td>
+      <td>
+        <?php print $resultRev['table_asigned'] ?>
+      </td>
+      <td>
+        <?php print $resultRev['num_person'] ?>        
+      </td>
+      <td class="text-primary">
+        Q.<?php print $resultRev['total_cost'] ?>        
+      </td>
+    </tr>
+  <?php
+  }
+  ?>
--- start/view/reservation.php
+++ start/view/reservation.php
@@ -61,8 +61,8 @@ if (isset($_SESSION['user'])) {
                           CONSUMO
                         </th>
                       </thead>
-                      <tbody>
-                        <?php $valWithoutFact = 0; printReservations($valWithoutFact); ?>                        
+                      <tbody id="reservations">
+                        <!-- FIXME: Vista de reservaciones en tiempo real -->                      
                       </tbody>
                     </table>
                   </div>
@@ -95,8 +95,8 @@ if (isset($_SESSION['user'])) {
                           CONSUMO
                         </th>
                       </thead>
-                      <tbody>
-                        <?php $valWithFact = 1; printReservations($valWithFact); ?>                        
+                      <tbody id="facts">
+                        <!-- FIXME: Vista de reservaciones facturadas en tiempo real -->                 
                       </tbody>
                     </table>
                   </div>
@@ -129,166 +129,31 @@ if (isset($_SESSION['user'])) {
   <!-- Material Dashboard DEMO methods, don't include it in your project! -->
   <script src="../assets/demo/demo.js"></script>
   <script>
-    $(document).ready(function() {
-      $().ready(function() {
-        $sidebar = $('.sidebar');
+    //Realizar la consuta cada tiempo para tener los datos frescos
+    //FIXME: Consulta de reservaciones vigentes
+    setInterval(() => {
+      var dataRev = $.ajax({
+        url: "../model/printReservations.php",
+        dataType: "text",
+        asyns: false
+      }).responseText
 
-        $sidebar_img_container = $sidebar.find('.sidebar-background');
+      var tableRev = document.getElementById('reservations')
+      tableRev.innerHTML = dataRev
+    }, 1000);
 
-        $full_page = $('.full-page');
+    //FIXME: Consulta de reservaciones facturadas
+    setInterval(() => {
+      var dataFact = $.ajax({
+        url: "../model/printFact.php",
+        dataType: "text",
+        async: false
+      }).responseText
 
-        $sidebar_responsive = $('body > .navbar-collapse');
+      var tableFact = document.getElementById('facts')
+      tableFact.innerHTML = dataFact
+    }, 1000);
 
-        window_width = $(window).width();
-
-        $('.fixed-plugin a').click(function(event) {
-          // Alex if we click on switch, stop propagation of the event, so the dropdown will not be hide, otherwise we set the  section active
-          if ($(this).hasClass('switch-trigger')) {
-            if (event.stopPropagation) {
-              event.stopPropagation();
-            } else if (window.event) {
-              window.event.cancelBubble = true;
-            }
-          }
-        });
-
-        $('.fixed-plugin .active-color span').click(function() {
-          $full_page_background = $('.full-page-background');
-
-          $(this).siblings().removeClass('active');
-          $(this).addClass('active');
-
-          var new_color = $(this).data('color');
-
-          if ($sidebar.length != 0) {
-            $sidebar.attr('data-color', new_color);
-          }
-
-          if ($full_page.length != 0) {
-            $full_page.attr('filter-color', new_color);
-          }
-
-          if ($sidebar_responsive.length != 0) {
-            $sidebar_responsive.attr('data-color', new_color);
-          }
-        });
-
-        $('.fixed-plugin .background-color .badge').click(function() {
-          $(this).siblings().removeClass('active');
-          $(this).addClass('active');
-
-          var new_color = $(this).data('background-color');
-
-          if ($sidebar.length != 0) {
-            $sidebar.attr('data-background-color', new_color);
-          }
-        });
-
-        $('.fixed-plugin .img-holder').click(function() {
-          $full_page_background = $('.full-page-background');
-
-          $(this).parent('li').siblings().removeClass('active');
-          $(this).parent('li').addClass('active');
-
-
-          var new_image = $(this).find("img").attr('src');
-
-          if ($sidebar_img_container.length != 0 && $('.switch-sidebar-image input:checked').length != 0) {
-            $sidebar_img_container.fadeOut('fast', function() {
-              $sidebar_img_container.css('background-image', 'url("' + new_image + '")');
-              $sidebar_img_container.fadeIn('fast');
-            });
-          }
-
-          if ($full_page_background.length != 0 && $('.switch-sidebar-image input:checked').length != 0) {
-            var new_image_full_page = $('.fixed-plugin li.active .img-holder').find('img').data('src');
-
-            $full_page_background.fadeOut('fast', function() {
-              $full_page_background.css('background-image', 'url("' + new_image_full_page + '")');
-              $full_page_background.fadeIn('fast');
-            });
-          }
-
-          if ($('.switch-sidebar-image input:checked').length == 0) {
-            var new_image = $('.fixed-plugin li.active .img-holder').find("img").attr('src');
-            var new_image_full_page = $('.fixed-plugin li.active .img-holder').find('img').data('src');
-
-            $sidebar_img_container.css('background-image', 'url("' + new_image + '")');
-            $full_page_background.css('background-image', 'url("' + new_image_full_page + '")');
-          }
-
-          if ($sidebar_responsive.length != 0) {
-            $sidebar_responsive.css('background-image', 'url("' + new_image + '")');
-          }
-        });
-
-        $('.switch-sidebar-image input').change(function() {
-          $full_page_background = $('.full-page-background');
-
-          $input = $(this);
-
-          if ($input.is(':checked')) {
-            if ($sidebar_img_container.length != 0) {
-              $sidebar_img_container.fadeIn('fast');
-              $sidebar.attr('data-image', '#');
-            }
-
-            if ($full_page_background.length != 0) {
-              $full_page_background.fadeIn('fast');
-              $full_page.attr('data-image', '#');
-            }
-
-            background_image = true;
-          } else {
-            if ($sidebar_img_container.length != 0) {
-              $sidebar.removeAttr('data-image');
-              $sidebar_img_container.fadeOut('fast');
-            }
-
-            if ($full_page_background.length != 0) {
-              $full_page.removeAttr('data-image', '#');
-              $full_page_background.fadeOut('fast');
-            }
-
-            background_image = false;
-          }
-        });
-
-        $('.switch-sidebar-mini input').change(function() {
-          $body = $('body');
-
-          $input = $(this);
-
-          if (md.misc.sidebar_mini_active == true) {
-            $('body').removeClass('sidebar-mini');
-            md.misc.sidebar_mini_active = false;
-
-            $('.sidebar .sidebar-wrapper, .main-panel').perfectScrollbar();
-
-          } else {
-
-            $('.sidebar .sidebar-wrapper, .main-panel').perfectScrollbar('destroy');
-
-            setTimeout(function() {
-              $('body').addClass('sidebar-mini');
-
-              md.misc.sidebar_mini_active = true;
-            }, 300);
-          }
-
-          // we simulate the window Resize so the charts will get updated in realtime.
-          var simulateWindowResize = setInterval(function() {
-            window.dispatchEvent(new Event('resize'));
-          }, 180);
-
-          // we stop the simulation of Window Resize after the animations are completed
-          setTimeout(function() {
-            clearInterval(simulateWindowResize);
-          }, 1000);
-
-        });
-      });
-    });
   </script>
 </body>
 </html>
