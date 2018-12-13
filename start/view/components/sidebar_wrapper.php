d6d2d302dc23559d6628e4ba4115d0f2ef4c9139
Andres Montenegro <41881835+andressmm4@users.noreply.github.com>
2018-12-12 19:40:44 -0600

    cambio de clase
    cambio de clase del menu lateral (no funcional)

--- start/view/components/sidebar_wrapper.php
+++ start/view/components/sidebar_wrapper.php
@@ -6,25 +6,25 @@
   </div>
   <div class="sidebar-wrapper">
     <ul class="nav">
-      <li class="nav-item active">
+      <li id="md" class="nav-item active">
         <a id="principal" class="nav-link" href="dashboard.php">
           <i class="material-icons">dashboard</i>
           <p>Panel</p>
         </a>
       </li>
-      <li class="nav-item ">
+      <li id="mt" class="nav-item ">
         <a id="tables" class="nav-link" href="tables.php">
           <i class="material-icons">table_chart</i>
           <p>Mesas</p>
         </a>
       </li>
-      <li class="nav-item ">
+      <li id="mc" class="nav-item ">
         <a id="consumo" class="nav-link" href="add.php">
           <i class="material-icons">create</i>
           <p>Consumo</p>
         </a>
       </li>
-      <li class="nav-item ">
+      <li id="mr" class="nav-item ">
         <a id="reserv" class="nav-link" href="reservation.php">
           <i class="material-icons">content_paste</i>
           <p>Reservaciones</p>
@@ -34,7 +34,7 @@
       if ($_SESSION['user'] != "admin") {
         // code...
       } else {
-      ?>
+        ?>
         <li class="nav-item active-pro ">
           <a class="nav-link" href="user_register.php">
             <i class="material-icons">people</i>
@@ -42,14 +42,45 @@
           </a>
         </li>
       <?php
-      }
-      ?>
+
+    }
+    ?>
     </ul>
   </div>
 </div>
 <script>
-  var dash 
-  var tb
-  var cons
-  var resv
+
+  cost mp = document.getElementById("principal")
+  cost mt = document.getElementById("principal")
+  cost mc = document.getElementById("principal")
+  cost mr = document.getElementById("principal")
+
+  document.getElementById("principal").addEventListener("click", =>{
+    mp.className += "active"
+    mt.className = "nav-item"
+    mc.className = "nav-item"
+    mr.className = "nav-item"
+  })
+
+  document.getElementById("tables").addEventListener("click", =>{
+    mp.className = "nav-item"
+    mt.className += "active"
+    mc.className = "nav-item"
+    mr.className = "nav-item"
+  })
+
+  document.getElementById("consumo").addEventListener("click", =>{
+    mp.className = "nav-item"
+    mt.className = "nav-item"
+    mc.className += "active"
+    mr.className = "nav-item"
+  })
+
+  document.getElementById("reserv").addEventListener("click", =>{
+    mp.className = "nav-item"
+    mt.className = "nav-item"
+    mc.className = "nav-item"
+    mr.className += "active"
+  })
+
 </script>
