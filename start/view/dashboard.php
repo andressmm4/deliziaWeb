f8ba063e037d213bef2c4398aab9a367ba215cd2
Andres Montenegro <41881835+andressmm4@users.noreply.github.com>
2018-12-12 19:52:32 -0600

    Cambio de Nombre

--- start/view/dashboard.php
+++ start/view/dashboard.php
@@ -13,7 +13,7 @@ if(isset($_SESSION['user'])){
   <link rel="icon" type="image/png" href="assets/img/store.png">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
   <title>
-    DELIZIA
+    DELIZI
   </title>
   <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
   <!--     Fonts and icons     -->
